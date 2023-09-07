638f9a85eace2c6a029c82793bb90ec5c4f8fc3e
urhol1 <martin.holcik@rieter.com>
2023-09-07 13:09:30 +0200

    [lely-canopen] updated port and SHA to 0 for now

--- ports/lely-canopen/portfile.cmake
+++ ports/lely-canopen/portfile.cmake
@@ -2,14 +2,14 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO MartinHolcik/lely-canopen
   REF e2ca7cb9e719e3371b88c3441d7f66e54a1a408d # pointer to commit (ID) of lely-canopen library repository
-  SHA512 6e81617b1eab5c5cc21bdb292d5d483b7ca7d58411aac31596d83ad54046208ab86b211b28045a71e44e0e6086603ec097dd25f00ec0b688da268f5b9af70f49
+  SHA512 0
   HEAD_REF master
 )
 
 vcpkg_configure_make(
   SOURCE_PATH ${SOURCE_PATH}
   AUTOCONFIG
-  BUILD_TRIPLET "--host=arm-linux-gnueabihf"
+  BUILD_TRIPLET "--host=x64" # arm-linux-gnueabihf"
   OPTIONS
   ${OPTIONS}
   --disable-python
